d0abd07fd128765e7dd5b433e3c2bd9cc184df3a
Oleg Ilin <www75@inbox.ru>
2021-01-24 05:32:35 +0300

    В файле 07_01_matrix.py изменил метод __str__. Сделал return вместо print

--- 07_01_matrix.py
+++ 07_01_matrix.py
@@ -3,20 +3,23 @@ class Matrix:
         self.arr = arr
 
     def __str__(self):
+        new_arr = ''
         for el in self.arr:
-            print(f'{el}')
+            new_arr += (str(el) + '\n')
+        return new_arr
 
     def __add__(self, other):
+        new_arr = ''
         for i in range(len(self.arr)):
             for j in range(len(self.arr[0])):
                 self.arr[i][j] = self.arr[i][j] + other.arr[i][j]
-        for result in self.arr:
-            print(result)
+        for el in self.arr:
+            new_arr = new_arr + (str(el) + '\n')
+        return new_arr
 
 
 m_1 = Matrix([[31, 22, 1], [37, 43, 1], [51, 86, 1]])
 m_2 = Matrix([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-m_1.__str__()
+print(m_1)
 print(m_1 + m_2)
-
 
